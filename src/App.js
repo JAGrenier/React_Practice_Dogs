8e5ddd8bb55bf4f2b9952c46089c28eeeabcbeb9
github username <github email address>
2020-10-07 16:26:09 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Card from './Component/Card';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  state = {
+    dogs: []
+  }
+  
+  handleClick = (event) => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then(response => response.json())
+      .then(data => this.setState({dogs: data.message}))
+    } 
+
+  render(){
+    return (
+      <div className="App">
+          <h1>Cute Doggos</h1>
+          <button onClick={this.handleClick}> Fetch A Cute Doggo</button>
+          <Card dogs={this.state.dogs} />
+      </div>
+    );
+  }
 }
 
 export default App;
