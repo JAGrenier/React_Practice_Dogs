d664190f1835ec7ba180dfc8b1b6258b47031807
github username <github email address>
2020-10-07 16:40:20 -0600

    added onclick

--- src/Component/Card.js
+++ src/Component/Card.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default function Card(props) {
+export default class Card extends Component{
 
+    state = {isClicked: false}
+    render(){
         return (
             <div className="Card">
-                <img src={props.dogs} />
+                <img 
+                src={this.props.dogs}
+                alt={"Random Dog Displays Here"}
+                onClick ={ () => this.setState({isClicked: !this.state.isClikced})}
+                />
+                {  this.state.isClicked
+                    ? <>
+                        <h2>Whose a good dog!</h2>
+                        <p> you're a good dog!</p> 
+                    </>
+                    : null 
+                }
             </div>
         )
-}
+    }
 
+}
